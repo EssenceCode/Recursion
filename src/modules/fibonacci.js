b48f5f15c655274669e60dd5fdae1d36c26885a2
EssenceCode <123456789+odin@users.noreply.github.com>
2023-10-27 10:08:36 +0800

    added the iterative and integer version of fibs

--- src/modules/fibonacci.js
+++ src/modules/fibonacci.js
@@ -1,19 +1,43 @@
-export default function fibsRec(n) {
-    if(n <= 1) return [0,1];
+/* eslint-disable no-unused-vars */
+export default function fibsRec(n, arr = [0,1]) {
+    if(n <= 1) return arr;
  
-     const arr = fibsRec(n - 1);
+    //  const arr = fibsRec(n - 1);
  
  
      arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
  
-     return arr;
+     return fibsRec(n - 1, arr);
  };
+
 // returns the array length 
-// eslint-disable-next-line no-unused-vars
 function fibsRecursion(n, arr = [0,1]) {
     if(n <= 2) return arr
 
     arr.push(arr[arr.length - 2] + arr[arr.length - 1])
 
     return fibsRecursion(n - 1, arr)
+}
+
+// loop
+function fibs(n) {
+    let firstEl = 0;
+    let secondEl = 1;
+    let total = 0;
+    const arr = []
+
+    for(let i = 1; i <= n; i += 1) {
+      total = firstEl + secondEl
+      secondEl = firstEl
+      firstEl = total
+      arr.push(total)
+    }
+
+    return arr
+}
+// integer 
+function fibsRecInt(n) {
+    if(n <= 1) return n;
+    console.log(n)
+    return fibsRecInt(n - 1) + fibsRecInt(n - 2)
 }
